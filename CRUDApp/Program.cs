d825b8aafce79739206e4efb30b412d68150b86d
jonty472 <azttorrs@gmail.com>
2023-07-16 11:06:15 +1200

    Add project files.

---
+++ CRUDApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace CRUDApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDatabaseConnection();
+        }
+
+
+
+        static void TestDatabaseConnection()
+        {
+            string connectionString = "";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            
+            connection.Open();
+
+        }
+    }
+}
